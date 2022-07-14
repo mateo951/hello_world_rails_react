8493542887a93b0115fd2c4d1f51c57ab846088c
Mateo Villagomez <mateovillagomez1995@gmail.com>
2022-07-14 12:03:08 -0500

    Add react-router

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,17 +1,15 @@
 import React from "react"
-import PropTypes from "prop-types"
-
-import { BroweserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import GreetingWorld from "./GreetingWorld";
 
 class App extends React.Component {
   render () {
     return (
-      <BroweserRouter>
+      <BrowserRouter>
         <Switch>
-          <Route exact path="/" render={() => <GreetingWorld greeting="This is a test"/> } />
+          <Route path="/" element={ <GreetingWorld greeting='Hello!' /> } />
         </Switch>
-      </BroweserRouter>
+      </BrowserRouter>
     );
   }
 }
